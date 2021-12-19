1ab77ff6b676863850533db0dfefe2f7c2c17fc1
tuandt2 <doanthanhtuan110793@gmail.com>
2021-12-19 20:42:31 +0700

    Update

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import scrollSpy from 'simple-scrollspy'
+import $ from "jquery";
+
+window.onload = function () {
+    scrollSpy('#main-menu', {
+      sectionClass: '.scrollspy',
+      menuActiveTarget: '.menu-item',
+      offset: 100,
+      scrollContainer: '.scroll-container',
+      // smooth scroll
+      smoothScroll: true,
+      smoothScrollBehavior: function(element) {
+        console.log('run "smoothScrollBehavior"...', element)
+        element.scrollIntoView({ behavior: 'smooth' })
+        calPosBorderCircle();
+      }
+   })
+}
+
+function calPosBorderCircle() {
+    $('.menu-item').each(function (index,v) {
+        let i = index + 1;
+        let _rotateDefault = -45;
+        const _rotatePlus = 25;
+        console.log(v)
+        if ($(v).hasClass('active')) {
+            _rotateDefault += _rotatePlus * i;
+            console.log(i, _rotateDefault);
+            $('.navbar svg').css('transform', 'rotate('+_rotateDefault+'deg)');
+        }
+    });
+}
