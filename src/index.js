534c2174770ce889fb3b8cecfeacac078b62d43d
Nguyen Dung <>
2022-01-15 15:47:40 +0700

    Fix: error

--- src/index.js
+++ src/index.js
@@ -1,32 +1,23 @@
-import scrollSpy from 'simple-scrollspy';
-import $ from "jquery";
+import $ from 'jquery';
+import scrollSpy from './libs/scrollspy';
+
+const arrRotate = [0, 45, 80, 125];
 
 window.onload = function () {
-    scrollSpy('#main-menu', {
-      sectionClass: '.scrollspy',
-      menuActiveTarget: '.menu-item',
-      offset: 100,
-      scrollContainer: '.scroll-container',
-      // smooth scroll
-      smoothScroll: true,
-      smoothScrollBehavior: function(element) {
-        console.log('run "smoothScrollBehavior"...', element)
-        element.scrollIntoView({ behavior: 'smooth' })
-        calPosBorderCircle();
-      }
-   })
-}
-
-function calPosBorderCircle() {
-    $('.menu-item').each(function (index,v) {
-        let i = index + 1;
-        let _rotateDefault = 0;
-        const _rotatePlus = 25;
-        console.log(v)
-        if ($(v).hasClass('active')) {
-            _rotateDefault += _rotatePlus * i;
-            console.log(i, _rotateDefault);
-            $('.navbar svg').css('transform', 'rotate('+_rotateDefault+'deg)');
-        }
-    });
+  scrollSpy('#main-menu', {
+    sectionClass: '.scrollspy',
+    menuActiveTarget: '.menu-item',
+    offset: 500,
+    scrollContainer: '.scroll-container',
+    smoothScroll: true,
+    smoothScrollBehavior: function(element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+      const indScroll = $(element).index() - 2;
+      $('.navbar svg').css('transform', `rotate(${arrRotate[indScroll]}deg)`);
+    },
+    onScrollChild: function(element) {
+      const indScroll = $(element).index();
+      $('.navbar svg').css('transform', `rotate(${arrRotate[indScroll]}deg)`);
+    }
+  });
 }
